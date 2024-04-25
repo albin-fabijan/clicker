6aea74a4a29614ec5a3373f699a85e693a55dcbd
helwan-djibril-mimouni <163547268+helwan-djibril-mimouni@users.noreply.github.com>
2024-04-25 15:23:10 +0200

    refactor(script.js): added localStorage logic

--- script.js
+++ script.js
@@ -4,6 +4,134 @@ let luck = 0
 
 let show = false
 let count = 0
+let setup_bool = true
+
+let money = null
+let num = null
+let money_num = null
+
+let interval_click = null
+let interval_luck = null
+
+function setup(){
+    if (setup_bool){
+        //pokemon
+        let pokemon_num_text = document.getElementById("pokemon_num")
+        let pkmn_num = 0
+        if (localStorage.getItem("num_pkmn") != null){
+            pkmn_num = parseInt(localStorage.getItem("num_pkmn"))
+        }
+        pokemon_num_text.innerText = pokemon_num_text.innerText.split(": ")[0] + ": " + (parseInt(pkmn_num))
+        let img = document.getElementById("enemy_img")
+        let evolve_price = document.getElementById("evolve_price")
+        if (pkmn_num < 649){
+            img.src = "sprites/black-white/" + (parseInt(pkmn_num)+1) + ".png"
+            evolve_price.innerText = "$" + Math.floor(100*(parseInt(pkmn_num)+1)*(parseInt(pkmn_num)+1)**((pkmn_num/100)+1))
+        }
+
+        //auto
+        let lvl_text = document.getElementById("auto_click_lvl")
+        let click_price = document.getElementById("auto_click_price")
+        let lvl = 0
+        if (localStorage.getItem("auto") != null){
+            lvl = localStorage.getItem("auto")
+        }
+        lvl_text.innerText = lvl_text.innerText.split("lvl ")[0] + "lvl " + lvl
+        if (lvl < 30){
+            lvl++
+            click_price.innerText = "$" + 10*((lvl)**2)
+            lvl--
+        }
+        else{
+            price = click_price.innerText.split('$')[1]
+            click_price.innerText = "$" + price*(lvl-28)**Math.floor(((lvl*2)/60))
+        }
+        if (lvl > 0){
+            interval_click = setInterval(() => {
+                if (5000/(lvl**2) > 10){
+                    money_num += 1
+                    localStorage.setItem("money", parseInt(money_num))
+                    money.innerText = "$" + getNumAbreviation(money_num) + ""
+                }
+                else{
+                    money_num += parseInt(lvl-22) * Math.floor(Math.floor(((lvl-22)**2)/lvl) + 1)
+                    localStorage.setItem("money", parseInt(money_num))
+                    money.innerText = "$" + getNumAbreviation(money_num) + ""
+                }
+            }, 5000/(lvl**2));
+        }
+
+        //click
+        let incr_text = document.getElementById("incr_num")
+        let incr_price = document.getElementById("click_incr_price")
+        let incr = 0
+        if (localStorage.getItem("incr") != null){
+            incr = localStorage.getItem("incr")
+        }
+        incr_text.innerText = incr_text.innerText.split("lvl ")[0] + "lvl " + incr
+        incr++
+        incr_price.innerText = "$" + 10*((incr)**2)
+        incr--
+
+        //luck
+        let luck_price = document.getElementById("luck_price")
+        if (localStorage.getItem("luck") != null){
+            luck = localStorage.getItem("luck")
+        }
+        luck++
+        luck++
+        luck_price.innerText = "$" + 10**(luck)
+        luck--
+        luck--
+        
+        //money
+        money = document.getElementById("money_text")
+        num = money.innerText.split('$')[1]
+        money_num = parseInt(num)
+        if (localStorage.getItem("money") != null){
+            money_num = parseInt(localStorage.getItem("money"))
+        }
+        money.innerText = "$" + getNumAbreviation(money_num) + ""
+        
+        setup_bool = false
+    }    
+}
+
+function reset(){
+    //money
+    money_num = 0
+    money.innerText = "$" + getNumAbreviation(money_num) + ""
+
+    //storage
+    localStorage.clear()
+
+    //pokemon
+    let pokemon_num_text = document.getElementById("pokemon_num")
+    pokemon_num_text.innerText = pokemon_num_text.innerText.split(": ")[0] + ": 0"
+    let img = document.getElementById("enemy_img")
+    img.src = "sprites/black-white/1.png"
+    let evolve_price = document.getElementById("evolve_price")
+    evolve_price.innerText = "$100"
+
+    //auto
+    clearInterval(interval_click)
+    let click_price = document.getElementById("auto_click_price")
+    click_price.innerText = "$10"
+    let lvl_text = document.getElementById("auto_click_lvl")
+    lvl_text.innerText = lvl_text.innerText.split("lvl ")[0] + "lvl 0"
+
+    //click
+    let incr_price = document.getElementById("click_incr_price")
+    incr_price.innerText = "$10"
+    let incr_text = document.getElementById("incr_num")
+    incr_text.innerText = incr_text.innerText.split("lvl ")[0] + "lvl 0"
+
+    //luck
+    clearInterval(interval_luck)
+    let luck_price = document.getElementById("luck_price")
+    luck_price.innerText = "$100"
+    luck = 0
+}
 
 let konami = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"]
 
@@ -28,10 +156,21 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 })
 
-function nextPokemon(){
-    let money = document.getElementById("money_text")
-    let num = money.innerText.split('$')[1]
+function getNumAbreviation(n){
+    if (n > 1000000){
+        print_num = Math.floor(n/1000000) + "." + Math.floor((n%1000000)/1000) + "M"
+        return print_num
+    }
+    else{
+        return n
+    }
+}
 
+setInterval(() => {
+    console.log(getNumAbreviation(money_num))
+}, 1);
+
+function nextPokemon(){
     let img = document.getElementById("enemy_img")
     let src = img.src + ""
     let slice1 = src.split('black-white/')[1] + ""
@@ -47,7 +186,7 @@ function nextPokemon(){
     let evolve_price = document.getElementById("evolve_price")
     price = evolve_price.innerText.split('$')[1]
     
-    if (parseInt(num) >= price){
+    if (parseInt(money_num) >= price){
         if (parseInt(id)+1 <= 649 && !shiny){
             img.src = "sprites/black-white/" + (parseInt(id)+1) + ".png"
             evolve_price.innerText = "$" + Math.floor(100*(parseInt(id)+1)*(parseInt(id)+1)**((id/100)+1))
@@ -55,13 +194,15 @@ function nextPokemon(){
         else if (parseInt(id) == 649 && !shiny){
             shiny = true
             img.src = "sprites/black-white/shiny/1.png"
-            evolve_price.innerText = "$" + 100*(parseInt(id)+1)*(parseInt(id)+1)*(parseInt(id))
+            evolve_price.innerText = "$" + Math.floor(100*(parseInt(id)+1)*(parseInt(id)+1)**((id/100)+1))
         }
         else{
             img.src = "sprites/black-white/shiny/" + (parseInt(id)+1) + ".png"
             evolve_price.innerText = "$" + 100*(parseInt(id)+650)**(Math.floor(id/10)+1)
         }
-        money.innerText = "$" + (parseInt(num) - price) + ""
+        money_num -= price
+        localStorage.setItem("money", parseInt(money_num))
+        money.innerText = "$" + getNumAbreviation(money_num) + ""
         total_money_spent += price
 
         if(localStorage.getItem("num_pkmn") != null){
@@ -69,7 +210,7 @@ function nextPokemon(){
         }
 
         let pokemon_num_text = document.getElementById("pokemon_num")
-        pokemon_num = pokemon_num_text.innerText.split(": ")[1]
+        let pokemon_num = pokemon_num_text.innerText.split(": ")[1]
         localStorage.setItem("num_pkmn", parseInt(pokemon_num)+1)
         pokemon_num_text.innerText = pokemon_num_text.innerText.split(": ")[0] + ": " + (parseInt(pokemon_num)+1)
     }
@@ -81,57 +222,53 @@ function getMoney(){
     let slice1 = src.split('black-white/')[1] + ""
     let id = slice1.split('.')[0]
 
-    let money = document.getElementById("money_text")
-    let num = money.innerText.split('$')[1]
-
     let incr_text = document.getElementById("incr_num")
     let incr = incr_text.innerText.split("lvl ")[1]
 
-    money.innerText = "$" + (parseInt(num)+(parseInt(id)+parseInt(incr))) + ""
+    money_num += parseInt(id)+parseInt(incr)
+    localStorage.setItem("money", parseInt(money_num))
+    money.innerText = "$" + getNumAbreviation(money_num) + ""
 
-    // Cloner l'élément audio
+    // Clone the audio element
     const clickSound = document.getElementById("clickSound");
     const clonedSound = clickSound.cloneNode();
     
-    // Jouer le son cloné
+    // Play the cloned sound
     clonedSound.play();
 }
 
 function next_click_incr(){
-    let money = document.getElementById("money_text")
-    let num = money.innerText.split('$')[1]
-
     let incr_price = document.getElementById("click_incr_price")
     price = incr_price.innerText.split('$')[1]
 
-    if (parseInt(num) >= price){
-        money.innerText = "$" + (parseInt(num) - price) + ""
+    if (money_num >= price){
+        money_num -= price
+        localStorage.setItem("money", parseInt(money_num))
+        money.innerText = "$" + getNumAbreviation(money_num) + ""
 
         let incr_text = document.getElementById("incr_num")
         let incr = incr_text.innerText.split("lvl ")[1]
         incr++
+        localStorage.setItem("incr", parseInt(incr))
         incr_text.innerText = incr_text.innerText.split("lvl ")[0] + "lvl " + incr
 
         incr_price.innerText = "$" + 10*((incr+1)**2)
     }
 }
 
-let interval_click = null
-let interval_luck = null
-
 async function next_auto_click(){
-    let money = document.getElementById("money_text")
-    let num = money.innerText.split('$')[1]
-
     let click_price = document.getElementById("auto_click_price")
     price = click_price.innerText.split('$')[1]
 
-    if (parseInt(num) >= price){
-        money.innerText = "$" + (parseInt(num) - price) + ""
+    if (money_num >= price){
+        money_num -= price
+        localStorage.setItem("money", parseInt(money_num))
+        money.innerText = "$" + getNumAbreviation(money_num) + ""
 
         let lvl_text = document.getElementById("auto_click_lvl")
         let lvl = lvl_text.innerText.split("lvl ")[1]
         lvl++
+        localStorage.setItem("auto", parseInt(lvl))
         lvl_text.innerText = lvl_text.innerText.split("lvl ")[0] + "lvl " + lvl
 
         if (lvl < 30){
@@ -146,30 +283,31 @@ async function next_auto_click(){
         interval_click = setInterval(() => {
             lvl = lvl_text.innerText.split("lvl ")[1]
 
-            let money = document.getElementById("money_text")
-            let num = money.innerText.split('$')[1]
-
             if (5000/(lvl**2) > 10){
-                money.innerText = "$" + (parseInt(num) + 1) + ""
+                money_num += 1
+                money.innerText = "$" + getNumAbreviation(money_num) + ""
+                localStorage.setItem("money", parseInt(money_num))
             }
             else{
-                money.innerText = "$" + (parseInt(num) + parseInt(lvl-22) * Math.floor(Math.floor(((lvl-22)**2)/lvl) + 1)) + ""
+                money_num += parseInt(lvl-22) * Math.floor(Math.floor(((lvl-22)**2)/lvl) + 1)
+                money.innerText = "$" + getNumAbreviation(money_num) + ""
+                localStorage.setItem("money", parseInt(money_num))
             }
         }, 5000/(lvl**2));
     }
 }
 
 async function next_luck(){
-    let money = document.getElementById("money_text")
-    let num = money.innerText.split('$')[1]
-
     let luck_price = document.getElementById("luck_price")
     price = luck_price.innerText.split('$')[1]
 
-    if (parseInt(num) >= price){
-        money.innerText = "$" + (parseInt(num) - price) + ""
+    if (money_num >= price){
+        money_num -= price
+        localStorage.setItem("money", parseInt(money_num))
+        money.innerText = "$" + getNumAbreviation(money_num) + ""
 
         luck++
+        localStorage.setItem("luck", parseInt(luck))
         
         luck_price.innerText = "$" + 10**(luck+2)
 
@@ -192,16 +330,16 @@ async function getPokedex(){
     let pokedex_div = document.getElementById("pokedex_div")
     pokedex_div.innerHTML = ""
 
-    let num = 0
+    let n = 0
 
     if(localStorage.getItem("num_pkmn") == null){
-        num = 1
+        n = 0
     }
     else{
-        num = localStorage.getItem("num_pkmn")
+        n = localStorage.getItem("num_pkmn")
     }
 
-    let array = new Array(parseInt(num)+1)
+    let array = new Array(parseInt(n)+1)
 
     for (let i = 0; i < array.length; i++){
         if (i < 649){
@@ -296,10 +434,9 @@ function lucky(){
 }
 
 function lucky_clicked(){
-    let money = document.getElementById("money_text")
-    let num = money.innerText.split('$')[1]
-
-    money.innerText = "$" + parseInt(num)*2 + ""
+    money_num *= 2
+    localStorage.setItem("money", parseInt(money_num))
+    money.innerText = "$" + parseInt(money_num) + ""
     
     let button = document.getElementById("lucky_button")
     button.remove()
